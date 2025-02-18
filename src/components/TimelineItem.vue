e325f041924a85cd53feca72804c7be52d457ff5
thebegood <str1k3sh00tqc@gmail.com>
2025-02-18 09:15:13 -0500

    first commit

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { type TimelinePost } from "../posts";
+//import Wishlist from "../../../../Exer06/my-app/src/components/Wishlist.vue";
+
+defineProps<{
+  post: TimelinePost;
+}>();
+</script>
+<template>
+  <li>
+    <span>{{ post.title }}</span
+    >&nbsp;|&nbsp;
+    <span>{{ post.created.toFormat("d MMM") }}</span>
+  </li>
+</template>
+<style></style>
